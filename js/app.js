240c4915e9c68e75fef646280a8c462aa814b467
vv0lny1 <rodion_raskolnikow@o2.pl>
2015-01-19 20:16:17 +0000

    firebase

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
-angular.module('myApp',[])
+angular.module('myApp',["firebase"])
+.constant('FIREBASE_URI','https://dazzling-inferno-1472.firebaseio.com/')
 .controller('myCtrl',function($scope,$http){
     var url = 'https://gdata.youtube.com/feeds/api/users/orbitalofficial/uploads?alt=json-in-script&callback=JSON_CALLBACK';
     $http.jsonp(url).success(function(data) {
@@ -9,4 +10,49 @@ angular.module('myApp',[])
     $scope.finder = function(entry){
         return ~entry.title.$t.toLowerCase().indexOf($scope.filtering.toLowerCase());
     };
+})
+
+.controller('firebaseCtrl',function($scope,ItemFactory){
+    $scope.items = ItemFactory.getItems();
+    
+    $scope.addItem = function(text){
+        ItemFactory.addItem({text:text});
+    };
+    
+    $scope.removeItem = function(item){
+        ItemFactory.removeItem(item);
+    };
+    
+})
+
+.factory('ItemFactory',function($firebase,FIREBASE_URI){
+    var ref = new Firebase(FIREBASE_URI);
+    ref = ref.child('items');
+    
+    var sync = $firebase(ref);
+    var items = sync.$asArray();
+    
+    var getItems = function () {
+        return items;
+    }
+    
+    var addItem = function(item){
+        items.$add(item);
+    }
+    
+    var removeItem = function(item){
+        console.log(item);
+        items.$remove(item);
+    }
+    
+    var updateItem = function(id){
+        
+    }
+    
+    return{
+        getItems: getItems,
+        addItem: addItem,
+        removeItem: removeItem
+    }
+    
 });
